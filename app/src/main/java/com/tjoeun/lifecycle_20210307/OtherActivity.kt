77b845e6a930dc607923c0a856696eced0fd3b23
조경진 <cho881020@gmail.com>
2021-03-07 10:37:17 +0900

    다른화면 > Base 상속으로 변경

--- app/src/main/java/com/tjoeun/lifecycle_20210307/OtherActivity.kt
+++ app/src/main/java/com/tjoeun/lifecycle_20210307/OtherActivity.kt
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class OtherActivity : AppCompatActivity() {
+class OtherActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+        setupEvents()
+        setValues()
+    }
 
-        Log.d("다른화면", "onCreate실행됨")
+    override fun setupEvents() {
 
     }
+
+    override fun setValues() {
+        Log.d("다른화면", "onCreate실행됨")
+    }
+
+
 }
